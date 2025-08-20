ae37eda6725aff0b9731068747f23c7b0a58763c
Barel Hajbi <barel26497@gmail.com>
2025-08-20 15:58:01 +0300

    add mongoose order schema

---
+++ shared/order.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    item: { type: String, required: true, trim: true },
+    amount: { type: Number, required: true, min: 1 },
+    status: { type: String, enum: ['Pending','Processed','Failed'], default: 'Pending' },
+    index: { type: Number, required: true, unique: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
